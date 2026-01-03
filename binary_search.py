74f40622e24bfbd43350af0ecea84f0b65aae317
Sandhyalaxmi J Bhat <sandhyalaxmijp@gmail.com>
2026-01-03 19:33:52 +0530

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,13 @@
+def binary_search(arr, x):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+print(binary_search([1, 2, 3, 4, 5], 4))
