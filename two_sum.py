0daad3465b2a612e12189c8653baa4ec6e9ac675
Sandhyalaxmi J Bhat <sandhyalaxmijp@gmail.com>
2026-01-03 19:33:07 +0530

    Create two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,8 @@
+def two_sum(nums, target):
+    d = {}
+    for i, num in enumerate(nums):
+        if target - num in d:
+            return [d[target - num], i]
+        d[num] = i
+
+print(two_sum([2, 7, 11, 15], 9))
